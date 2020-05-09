912b6318f7feba84882e6ec2ecb0353ebede3359
kiran.varghese <kiran.varghese@INDCHN-96Z1ZY2>
2020-05-10 01:44:14 +0530

    Adding DI

---
+++ AppManager/Processors/TokenProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using AppManager.AppInterfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AppManager.Processors
+{
+    public class TokenProcessor : ITokenProcessor
+    {
+        public string createNewWebToken()
+        {
+            //the secret Key
+            string secretKey = "Hello_secret_key";
+            //Create symetric key 
+            var symetricSecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+            //sign the token
+            var signingCredentails = new SigningCredentials(symetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+            //Create the token
+
+            var token = new JwtSecurityToken(issuer: "KiransComputer", audience: "someReceiver", expires: DateTime.Now.AddMinutes(1), signingCredentials: signingCredentails);
+
+
+            //write the token
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public void ValidateWebToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- First.Net.Core.Api/Controllers/AuthController.cs
+++ First.Net.Core.Api/Controllers/AuthController.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using AppManager.Processors;
+using AppManager.AppInterfaces;
 
 namespace First.Net.Core.Api.Controllers
 {
@@ -14,21 +16,14 @@ namespace First.Net.Core.Api.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        public IActionResult GetToken()
+        private ITokenProcessor itokenprocessor;
+        public AuthController(ITokenProcessor itokenprocessor)
         {
-            //the secret Key
-            string secretKey = "Hello_secret_key";
-            //Create symetric key 
-            var symetricSecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
-            //sign the token
-            var signingCredentails = new SigningCredentials(symetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
-            //Create the token
-
-            var token = new JwtSecurityToken(issuer:"KiransComputer", audience: "someReceiver", expires:DateTime.Now.AddMinutes(1), signingCredentials: signingCredentails);
-            
-            
-            //write the token
-            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+            this.itokenprocessor = itokenprocessor;
+        }
+        public IActionResult Post()
+        {
+            return Ok(itokenprocessor.createNewWebToken());
         }
     }
 }
